a6f3ba49534981eab92d799e52373fec21deaf70
Gonalgar <gonzalo.alvarez@alumnos.upm.es>
2025-01-27 13:00:54 +0100

    Add functionality to nav bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+import { loadHome } from "./home";
+import { loadMenu } from "./menu";
+import { loadAbout } from "./about";
+
+const homeButton = document.getElementById("home");
+const menuButton = document.getElementById("menu");
+const aboutButton = document.getElementById("about");
+
+homeButton.addEventListener("click", loadHome);
+menuButton.addEventListener("click", loadMenu);
+aboutButton.addEventListener("click", loadAbout);
